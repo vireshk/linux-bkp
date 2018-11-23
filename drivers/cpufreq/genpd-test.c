d60bc4ffec6d396efd2309a7a4b90d5eee1ccbb8
Viresh Kumar <viresh.kumar@linaro.org>
2018-11-23 14:50:16 +0530

    TESTING: provider with multiple domains
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/genpd-test.c
+++ drivers/cpufreq/genpd-test.c
@@ -29,6 +29,12 @@ static int pd_set_performance_parent(struct generic_pm_domain *domain, unsigned 
 	return 0;
 }
 
+static int pd_set_performance_multi(struct generic_pm_domain *domain, unsigned int state)
+{
+	pr_info("%s: %d: %d\n", __func__, __LINE__, state);
+	return 0;
+}
+
 static int pd_set_performance(struct generic_pm_domain *domain, unsigned int state)
 {
 	return 0;
@@ -70,38 +76,90 @@ static const struct of_device_id pm_domain_of_match[] __initconst = {
 	{ },
 };
 
+static const struct of_device_id pm_domain_of_match_multi[] __initconst = {
+	{
+		.compatible = "foo_multi,genpd",
+		.data = pd_set_performance_multi,
+	},
+	{ },
+};
+
+static struct generic_pm_domain *pd_init(struct device_node *np, const struct of_device_id *of_id)
+{
+	struct generic_pm_domain *pd;
+
+	pd = kzalloc(sizeof(*pd), GFP_KERNEL);
+	if (!pd)
+		return NULL;
+
+	pd->name = kstrdup_const(np->full_name, GFP_KERNEL);
+	if (!pd->name) {
+		of_node_put(np);
+		return NULL;
+	}
+
+	pd->power_off = pd_power_off;
+	pd->power_on = pd_power_on;
+
+	pd->set_performance_state = of_id->data;
+	pd->opp_to_performance_state = pd_get_performance;
+
+	pm_genpd_init(pd, NULL, false);
+	return pd;
+}
+
 static int __init add_domains(void)
 {
 	struct device_node *np;
 	struct generic_pm_domain *pd, *pd_ptr[3];
 	const struct of_device_id *of_id;
+	struct genpd_onecell_data *data;
 	int i = 0;
 
 	for_each_matching_node_and_match(np, pm_domain_of_match, &of_id) {
-		pd = kzalloc(sizeof(*pd), GFP_KERNEL);
+		pd = pd_init(np, of_id);
 		if (!pd)
 			return -ENOMEM;
 
-		pd->name = kstrdup_const(np->full_name, GFP_KERNEL);
-		if (!pd->name) {
-			of_node_put(np);
-			return -1;
-		}
-
-		pd->power_off = pd_power_off;
-		pd->power_on = pd_power_on;
-
-		pd->set_performance_state = of_id->data;
-		pd->opp_to_performance_state = pd_get_performance;
-
-		pm_genpd_init(pd, NULL, false);
 		of_genpd_add_provider_simple(np, pd);
 
 		pd_ptr[i++] = pd;
 	}
 
-	i = pm_genpd_add_subdomain(pd_ptr[0], pd_ptr[1]);
-	i = pm_genpd_add_subdomain(pd_ptr[0], pd_ptr[2]);
+	if (i > 1) {
+		i = pm_genpd_add_subdomain(pd_ptr[0], pd_ptr[1]);
+		i = pm_genpd_add_subdomain(pd_ptr[0], pd_ptr[2]);
+	}
+
+	i = 0;
+
+	for_each_matching_node_and_match(np, pm_domain_of_match_multi, &of_id) {
+		data = kzalloc(sizeof(*data), GFP_KERNEL);
+		if (!data)
+			return -ENOMEM;
+
+		data->domains = kcalloc(2, sizeof(*data->domains), GFP_KERNEL);
+		if (!data->domains)
+			return -ENOMEM;
+
+		data->num_domains = 2;
+
+again:
+		pd = pd_init(np, of_id);
+		if (!pd)
+			return -ENOMEM;
+
+		data->domains[i] = pd;
+
+		if (++i == 1)
+			goto again;
+
+		of_genpd_add_provider_onecell(np, data);
+
+		pm_genpd_add_subdomain(pd_ptr[0], data->domains[0]);
+		pm_genpd_add_subdomain(pd_ptr[0], data->domains[1]);
+	}
+
 	return 0;
 }
 
