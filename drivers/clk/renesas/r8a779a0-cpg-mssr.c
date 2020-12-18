320001fb73a1c07a07af4dd81547dfce620717e8
Wolfram Sang <wsa+renesas@sang-engineering.com>
2020-12-18 10:50:49 +0100

    clk: renesas: r8a779a0: Add RWDT clocks
    And introduce critical clocks, too, because RWDT is one.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- drivers/clk/renesas/r8a779a0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779a0-cpg-mssr.c
@@ -188,6 +188,7 @@ static const struct mssr_mod_clk r8a779a0_mod_clks[] __initconst = {
 	DEF_MOD("vin35",	827,	R8A779A0_CLK_S1D1),
 	DEF_MOD("vin36",	828,	R8A779A0_CLK_S1D1),
 	DEF_MOD("vin37",	829,	R8A779A0_CLK_S1D1),
+	DEF_MOD("rwdt",		907,	R8A779A0_CLK_R),
 };
 
 static spinlock_t cpg_lock;
@@ -261,6 +262,10 @@ static struct clk * __init rcar_r8a779a0_cpg_clk_register(struct device *dev,
 					 __clk_get_name(parent), 0, mult, div);
 }
 
+static const unsigned int r8a779a0_crit_mod_clks[] __initconst = {
+	MOD_CLK_ID(907),	/* RWDT */
+};
+
 /*
  * CPG Clock Data
  */
@@ -310,6 +315,10 @@ const struct cpg_mssr_info r8a779a0_cpg_mssr_info __initconst = {
 	.mod_clks = r8a779a0_mod_clks,
 	.num_mod_clks = ARRAY_SIZE(r8a779a0_mod_clks),
 	.num_hw_mod_clks = 15 * 32,
+
+	/* Critical Module Clocks */
+	.crit_mod_clks		= r8a779a0_crit_mod_clks,
+	.num_crit_mod_clks	= ARRAY_SIZE(r8a779a0_crit_mod_clks),
 
 	/* Callbacks */
 	.init = r8a779a0_cpg_mssr_init,
