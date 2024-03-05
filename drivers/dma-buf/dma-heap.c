f4e3be5a8586206537741b2a1a2b4cc98cbc5c0a
Ricardo B. Marliere <ricardo@marliere.net>
2024-03-05 08:34:13 -0300

    dma-buf: heaps: make dma_heap_class constant
    Since commit 43a7206b0963 ("driver core: class: make class_register() take
    a const *"), the driver core allows for struct class to be in read-only
    memory, so move the dma_heap_class structure to be declared at build time
    placing it into read-only memory, instead of having to be dynamically
    allocated at boot time.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Suggested-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: "Ricardo B. Marliere" <ricardo@marliere.net>
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Reviewed-by: "T.J. Mercier" <tjmercier@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/dma-buf/dma-heap.c
+++ drivers/dma-buf/dma-heap.c
@@ -42,10 +42,18 @@ struct dma_heap {
 	struct cdev heap_cdev;
 };
 
+static char *dma_heap_devnode(const struct device *dev, umode_t *mode)
+{
+	return kasprintf(GFP_KERNEL, "dma_heap/%s", dev_name(dev));
+}
+
 static LIST_HEAD(heap_list);
 static DEFINE_MUTEX(heap_list_lock);
 static dev_t dma_heap_devt;
-static struct class *dma_heap_class;
+static struct class dma_heap_class = {
+	.name = DEVNAME,
+	.devnode = dma_heap_devnode,
+};
 static DEFINE_XARRAY_ALLOC(dma_heap_minors);
 
 static int dma_heap_buffer_alloc(struct dma_heap *heap, size_t len,
@@ -264,7 +272,7 @@ struct dma_heap *dma_heap_add(const struct dma_heap_export_info *exp_info)
 		goto err1;
 	}
 
-	dev_ret = device_create(dma_heap_class,
+	dev_ret = device_create(&dma_heap_class,
 				NULL,
 				heap->heap_devt,
 				NULL,
@@ -294,7 +302,7 @@ struct dma_heap *dma_heap_add(const struct dma_heap_export_info *exp_info)
 	return heap;
 
 err3:
-	device_destroy(dma_heap_class, heap->heap_devt);
+	device_destroy(&dma_heap_class, heap->heap_devt);
 err2:
 	cdev_del(&heap->heap_cdev);
 err1:
@@ -302,11 +310,6 @@ err1:
 err0:
 	kfree(heap);
 	return err_ret;
-}
-
-static char *dma_heap_devnode(const struct device *dev, umode_t *mode)
-{
-	return kasprintf(GFP_KERNEL, "dma_heap/%s", dev_name(dev));
 }
 
 static int dma_heap_init(void)
@@ -317,12 +320,11 @@ static int dma_heap_init(void)
 	if (ret)
 		return ret;
 
-	dma_heap_class = class_create(DEVNAME);
-	if (IS_ERR(dma_heap_class)) {
+	ret = class_register(&dma_heap_class);
+	if (ret) {
 		unregister_chrdev_region(dma_heap_devt, NUM_HEAP_MINORS);
-		return PTR_ERR(dma_heap_class);
+		return ret;
 	}
-	dma_heap_class->devnode = dma_heap_devnode;
 
 	return 0;
 }
