96c90f5f2b7d40dc04134c11ed7e6de629672cbb
Edgar E. Iglesias <edgar.iglesias@amd.com>
2025-11-25 18:33:05 +0100

    virtio-msg-sapphire: Add user-space device queue
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

--- drivers/virtio/virtio_msg_sapphire.c
+++ drivers/virtio/virtio_msg_sapphire.c
@@ -11,6 +11,9 @@
 #include <linux/hrtimer.h>
 #include <linux/pci.h>
 #include <linux/completion.h>
+#include <linux/list.h>
+#include <linux/iopoll.h>
+#include <linux/mm.h>
 
 #include "virtio_msg_amp.h"
 
@@ -32,7 +35,220 @@ struct sapphire_dev {
 	dma_addr_t shmem_dma;
 
 	bool probed_ok;
+
+	struct virtio_msg_user_device vmudev;
+	struct spsc_queue user_drv2dev;
+	struct spsc_queue user_dev2drv;
+	spinlock_t user_lock;
+	struct list_head user_pending;
+	struct sapphire_user_msg *user_current;
+	bool user_registered;
+	dma_addr_t user_phys;
+	size_t user_size;
+	resource_size_t bar3_start;
+	resource_size_t bar3_size;
 };
+
+struct sapphire_user_msg {
+	struct list_head list;
+	u16 len;
+	struct virtio_msg msg;
+};
+
+#define SAPPHIRE_CFG_OFFSET	(0x4000 / sizeof(u32))
+#define SAPPHIRE_CFG_READY	(SAPPHIRE_CFG_OFFSET + 0)
+#define SAPPHIRE_CFG_ADDR_LO	(SAPPHIRE_CFG_OFFSET + 1)
+#define SAPPHIRE_CFG_ADDR_HI	(SAPPHIRE_CFG_OFFSET + 2)
+
+#define SAPPHIRE_PAGE_SIZE	SZ_4K
+#define SAPPHIRE_MSG_BUF_SIZE	64
+
+static int sapphire_tx_notify(struct virtio_msg_amp *_amp_dev, u32 notify_idx);
+
+static inline struct sapphire_dev *vmudev_to_sapphire(struct virtio_msg_user_device *vmudev)
+{
+	return container_of(vmudev, struct sapphire_dev, vmudev);
+}
+
+static int sapphire_cfg_queue(struct sapphire_dev *sapphire_dev,
+				dma_addr_t phys, u32 ready)
+{
+	u32 val;
+
+	writel(lower_32_bits(phys), &sapphire_dev->cfg_bram[SAPPHIRE_CFG_ADDR_LO]);
+	writel(upper_32_bits(phys), &sapphire_dev->cfg_bram[SAPPHIRE_CFG_ADDR_HI]);
+	wmb();
+	writel(ready, &sapphire_dev->cfg_bram[SAPPHIRE_CFG_READY]);
+	wmb();
+
+	return readl_poll_timeout(&sapphire_dev->cfg_bram[SAPPHIRE_CFG_READY],
+				val, val == 0, 1, 1000);
+}
+
+static struct sapphire_user_msg *sapphire_user_msg_alloc(const struct virtio_msg *msg,
+					      u16 len, gfp_t gfp)
+{
+	size_t alloc = sizeof(struct sapphire_user_msg) + len - sizeof(struct virtio_msg);
+	struct sapphire_user_msg *node;
+
+	node = kzalloc(alloc, gfp);
+	if (!node)
+		return NULL;
+
+	node->len = len;
+	memcpy(&node->msg, msg, len);
+
+	return node;
+}
+
+static void sapphire_user_try_deliver_locked(struct sapphire_dev *sapphire_dev)
+{
+	struct sapphire_user_msg *node;
+
+	if (sapphire_dev->user_current)
+		return;
+
+	if (READ_ONCE(sapphire_dev->vmudev.vmsg))
+		return;
+
+	if (list_empty(&sapphire_dev->user_pending))
+		return;
+
+	node = list_first_entry(&sapphire_dev->user_pending,
+				struct sapphire_user_msg, list);
+	list_del_init(&node->list);
+	sapphire_dev->user_current = node;
+	WRITE_ONCE(sapphire_dev->vmudev.vmsg, &node->msg);
+	complete(&sapphire_dev->vmudev.r_completion);
+	wake_up_interruptible(&sapphire_dev->vmudev.poll_wq);
+}
+
+static void sapphire_user_enqueue_rx(struct sapphire_dev *sapphire_dev,
+				      struct virtio_msg *msg, u16 len,
+				      gfp_t gfp)
+{
+	struct sapphire_user_msg *node;
+	unsigned long flags;
+
+	node = sapphire_user_msg_alloc(msg, len, gfp);
+	if (!node)
+		return;
+
+	spin_lock_irqsave(&sapphire_dev->user_lock, flags);
+	list_add_tail(&node->list, &sapphire_dev->user_pending);
+	sapphire_user_try_deliver_locked(sapphire_dev);
+	spin_unlock_irqrestore(&sapphire_dev->user_lock, flags);
+}
+
+static void sapphire_user_process_rx(struct sapphire_dev *sapphire_dev)
+{
+	u8 buf[SAPPHIRE_MSG_BUF_SIZE];
+	struct virtio_msg *msg = (struct virtio_msg *)buf;
+	u32 len;
+
+	while (spsc_recv(&sapphire_dev->user_dev2drv, buf, sizeof(buf))) {
+		len = le16_to_cpu(msg->msg_size);
+		if (!len)
+			len = VIRTIO_MSG_MIN_SIZE;
+		len = min_t(u32, len, VIRTIO_MSG_MAX_SIZE);
+		len = min_t(u32, len, (u32)sizeof(buf));
+		sapphire_user_enqueue_rx(sapphire_dev, msg, (u16)len, GFP_ATOMIC);
+	}
+}
+
+static int sapphire_user_handle(struct virtio_msg_user_device *vmudev,
+				   struct virtio_msg *msg)
+{
+	struct sapphire_dev *sapphire_dev = vmudev_to_sapphire(vmudev);
+	u32 len = le16_to_cpu(msg->msg_size);
+
+	if (!len)
+		len = VIRTIO_MSG_MIN_SIZE;
+	len = min_t(u32, len, VIRTIO_MSG_MAX_SIZE);
+	len = min_t(u32, len, (u32)SAPPHIRE_MSG_BUF_SIZE);
+
+	if (!spsc_send(&sapphire_dev->user_drv2dev, msg, len))
+		return -EBUSY;
+
+	smp_wmb();
+	sapphire_tx_notify(&sapphire_dev->amp_dev, 0);
+
+	return 0;
+}
+
+static void sapphire_user_refill(struct virtio_msg_user_device *vmudev)
+{
+	struct sapphire_dev *sapphire_dev = vmudev_to_sapphire(vmudev);
+	struct sapphire_user_msg *node;
+	unsigned long flags;
+
+	spin_lock_irqsave(&sapphire_dev->user_lock, flags);
+	node = sapphire_dev->user_current;
+	sapphire_dev->user_current = NULL;
+	WRITE_ONCE(vmudev->vmsg, NULL);
+	if (node)
+		kfree(node);
+	sapphire_user_try_deliver_locked(sapphire_dev);
+	spin_unlock_irqrestore(&sapphire_dev->user_lock, flags);
+}
+
+static int sapphire_user_mmap(struct virtio_msg_user_device *vmudev,
+			    struct vm_area_struct *vma)
+{
+	struct sapphire_dev *sapphire_dev = vmudev_to_sapphire(vmudev);
+	unsigned long size = vma->vm_end - vma->vm_start;
+	resource_size_t offset = (resource_size_t)vma->vm_pgoff << PAGE_SHIFT;
+	resource_size_t phys;
+
+	if (!sapphire_dev->bar3_size)
+		return -ENODEV;
+
+	if (offset >= sapphire_dev->bar3_size)
+		return -EINVAL;
+
+	if (size > sapphire_dev->bar3_size - offset)
+		return -EINVAL;
+
+	phys = sapphire_dev->bar3_start + offset;
+	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+	vm_flags_set(vma, VM_IO | VM_DONTEXPAND | VM_DONTDUMP);
+
+	if (remap_pfn_range(vma, vma->vm_start, phys >> PAGE_SHIFT,
+			 size, vma->vm_page_prot))
+		return -EAGAIN;
+
+	return 0;
+}
+
+static struct virtio_msg_user_ops sapphire_user_uops = {
+	.handle = sapphire_user_handle,
+	.refill = sapphire_user_refill,
+};
+
+static void sapphire_user_cleanup(struct sapphire_dev *sapphire_dev)
+{
+	struct sapphire_user_msg *node;
+	unsigned long flags;
+
+	if (!sapphire_dev->user_registered)
+		return;
+
+	virtio_msg_user_unregister(&sapphire_dev->vmudev);
+	sapphire_dev->user_registered = false;
+
+	spin_lock_irqsave(&sapphire_dev->user_lock, flags);
+	if (sapphire_dev->user_current) {
+		kfree(sapphire_dev->user_current);
+		sapphire_dev->user_current = NULL;
+	}
+	while (!list_empty(&sapphire_dev->user_pending)) {
+		node = list_first_entry(&sapphire_dev->user_pending,
+					 struct sapphire_user_msg, list);
+		list_del(&node->list);
+		kfree(node);
+	}
+	spin_unlock_irqrestore(&sapphire_dev->user_lock, flags);
+}
 
 /**
  *  sapphire_irq_handler: IRQ from our PCI device
@@ -46,6 +262,8 @@ static irqreturn_t sapphire_irq_handler(int irq, void *dev_id)
 	err = virtio_msg_amp_notify_rx(&sapphire_dev->amp_dev, 0);
 	if (err)
 		dev_err(&sapphire_dev->pdev->dev, "sapphire IRQ error %d", err);
+
+	sapphire_user_process_rx(sapphire_dev);
 
 	return IRQ_HANDLED;
 }
@@ -110,6 +328,8 @@ static enum hrtimer_restart sapphire_poll_timer_expired(struct hrtimer *hrtimer)
 	if (err)
 		dev_err(&sapphire_dev->pdev->dev, "sapphire NOTIFY error %d", err);
 
+	sapphire_user_process_rx(sapphire_dev);
+
 	hrtimer_forward_now(hrtimer, ms_to_ktime(50));
         return HRTIMER_RESTART;
 }
@@ -117,13 +337,14 @@ static enum hrtimer_restart sapphire_poll_timer_expired(struct hrtimer *hrtimer)
 static int sapphire_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
 	struct sapphire_dev *sapphire_dev;
-	int err, irq;
+	int err, irq, ret;
 	const char *device_name;
 	const char *name;
 	phys_addr_t addr;
 	resource_size_t	size;
 	void *bar;
 	u64 bar64;
+	char *shmem;
 
 	printk("%s\n", __func__);
 	sapphire_dev = devm_kzalloc(&pdev->dev, sizeof(struct sapphire_dev),
@@ -156,6 +377,12 @@ static int sapphire_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	addr = pci_resource_start(pdev, 0);
 	size = pci_resource_len(pdev, 0);
 	dev_info(&pdev->dev, "%s at %pa, size %pa\n", name, &addr, &size);
+
+	addr = pci_resource_start(pdev, 2);
+	size = pci_resource_len(pdev, 2);
+	sapphire_dev->bar3_start = addr;
+	sapphire_dev->bar3_size = size;
+	dev_info(&pdev->dev, "BAR3 (user window) at %pa, size %pa\n", &addr, &size);
 
 	name = "shmem (BAR2)";
 	addr = pci_resource_start(pdev, 1);
@@ -203,14 +430,43 @@ static int sapphire_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	pci_set_master(pdev);
 
 	/* dma map shmem.  */
-	sapphire_dev->amp_dev.shmem = dma_alloc_coherent(&pdev->dev, 8 * 1024,
-			                                 &sapphire_dev->shmem_dma,
-                                                         GFP_KERNEL);
-	sapphire_dev->amp_dev.shmem_size = 8 * 1024;
+	sapphire_dev->amp_dev.shmem = dma_alloc_coherent(&pdev->dev, 16 * 1024,
+				                                 &sapphire_dev->shmem_dma,
+	                                                        GFP_KERNEL);
+	sapphire_dev->amp_dev.shmem_size = 16 * 1024;
 	memset(sapphire_dev->amp_dev.shmem, 0, sapphire_dev->amp_dev.shmem_size);
 	printk("%s: shmem=%p %llx\n", __func__,
             sapphire_dev->amp_dev.shmem,
             sapphire_dev->shmem_dma);
+
+	spin_lock_init(&sapphire_dev->user_lock);
+	INIT_LIST_HEAD(&sapphire_dev->user_pending);
+	sapphire_dev->user_current = NULL;
+	sapphire_dev->user_registered = false;
+
+	sapphire_dev->user_phys = sapphire_dev->shmem_dma + 2 * SAPPHIRE_PAGE_SIZE;
+	sapphire_dev->user_size = 2 * SAPPHIRE_PAGE_SIZE;
+
+	shmem = sapphire_dev->amp_dev.shmem;
+
+	spsc_init(&sapphire_dev->user_drv2dev, "user-drv2dev",
+		  spsc_capacity(SAPPHIRE_PAGE_SIZE),
+		  shmem + 2 * SAPPHIRE_PAGE_SIZE);
+	spsc_init(&sapphire_dev->user_dev2drv, "user-dev2drv",
+		  spsc_capacity(SAPPHIRE_PAGE_SIZE),
+		  shmem + 3 * SAPPHIRE_PAGE_SIZE);
+
+	sapphire_dev->vmudev.ops = &sapphire_user_uops;
+	sapphire_dev->vmudev.parent = &pdev->dev;
+	sapphire_dev->vmudev.mmap = sapphire_user_mmap;
+
+ret = virtio_msg_user_register(&sapphire_dev->vmudev);
+if (ret) {
+	err = ret;
+	goto error_user_register;
+}
+
+	sapphire_dev->user_registered = true;
 
 	dev_info(&pdev->dev, "SHMEM @ 0: %32ph \n", sapphire_dev->amp_dev.shmem);
 
@@ -226,11 +482,15 @@ static int sapphire_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (err)
 		goto error_reg;
 
+	addr = sapphire_dev->user_phys;
+	ret = sapphire_cfg_queue(sapphire_dev, addr, 1);
+	if (ret)
+		dev_warn(&pdev->dev, "Timeout configuring userspace queue\n");
+
 	addr = sapphire_dev->shmem_dma;
-	sapphire_dev->cfg_bram[0x4000/4 + 1] = addr;
-	sapphire_dev->cfg_bram[0x4000/4 + 2] = addr >> 32;
-	smp_wmb();
-	sapphire_dev->cfg_bram[0x4000/4 + 0] = 1;
+	ret = sapphire_cfg_queue(sapphire_dev, addr, 2);
+	if (ret)
+		dev_warn(&pdev->dev, "Timeout configuring kernel queue\n");
 
 	sapphire_dev->probed_ok = true;
 
@@ -238,10 +498,14 @@ static int sapphire_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 	return 0;
 
+error_user_register:
+	goto error_reg;
+
 error_reg:
+	sapphire_user_cleanup(sapphire_dev);
 	printk("free coherent\n");
-	dma_free_coherent(&pdev->dev, 8 * 1024,
-			 sapphire_dev->amp_dev.shmem, sapphire_dev->shmem_dma);
+	dma_free_coherent(&pdev->dev, 16 * 1024,
+		 sapphire_dev->amp_dev.shmem, sapphire_dev->shmem_dma);
 
 	printk("free coherent done\n");
 	pci_clear_master(pdev);
@@ -265,6 +529,8 @@ static void sapphire_remove(struct pci_dev *pdev)
 	writel(0, &sapphire_dev->regs->int_status);
 	pci_clear_master(pdev);
 
+	sapphire_user_cleanup(sapphire_dev);
+
 	virtio_msg_amp_unregister(&sapphire_dev->amp_dev);
 
 	for (i = 0; i < sapphire_dev->vectors; i++)
@@ -281,6 +547,7 @@ static void sapphire_shutdown(struct pci_dev *pdev)
 
 	/* Need to tell our virtio-msg peer we're going down.  */
 	virtio_msg_amp_unregister(&sapphire_dev->amp_dev);
+	sapphire_user_cleanup(sapphire_dev);
 	pci_clear_master(pdev);
 }
 
